19f665c2f6224de6fa9f51eb79404c730b3cb2cc
jojozagjos <josephgslade@gmail.com>
2024-11-25 09:23:39 -0700

    updated How to use? page

--- src/app/info/page.js
+++ src/app/info/page.js
@@ -3,18 +3,26 @@
 import React from 'react';
 import Sidebar from '@/app/components/Sidebar';
 
-const info = () => {
+const Info = () => {
     return (
         <div className="flex flex-col h-screen">
             <div className="flex flex-1">
                 <Sidebar />
-                <div className="p-4 flex-grow">
-                    <h1 className="text-2xl font-bold">How to Use DinoByte</h1>
-                    <p className="mt-4 text-lg">This is the "How to Use" page where you can explain your application's features and guide users on how to interact with it.</p>
+                <div className="p-4 flex-grow flex items-center justify-center">
+                    <div className="text-center">
+                        <h1 className="text-3xl font-bold">How to Use DinoByte</h1>
+                        <p className="mt-4 text-lg">To enjoy music seamlessly while using DinoByte, follow these steps to connect Spotify to DinoByte:</p>
+                        <ol className="text-lg font-semibold space-y-6 mt-4">
+                            <li>Open Spotify in a new tab or the Spotify application.</li>
+                            <li>Click on the <strong className="text-blue-500">Connect to Device</strong> button.</li>
+                            <li>Select <strong className="text-blue-500">DinoByte</strong> from the list of available devices.</li>
+                            <li>Enjoy listening to music on <strong className="text-blue-500">DinoByte</strong>!</li>
+                        </ol>
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
 
-export default info;
+export default Info;
